ddab1ec6e1a6d6f57fddb4de3b96c8131b3a3d59
SMULL <smull.st@gmai.com>
2019-04-07 18:36:01 +0300

    Elevator system

--- src/main/java/com/sprinklebit/ElevatorConfiguration.java
+++ src/main/java/com/sprinklebit/ElevatorConfiguration.java
@@ -2,13 +2,13 @@ package com.sprinklebit;
 
 public interface ElevatorConfiguration
 {
-    public static final int NUMBER_OF_FLOORS = 4;
-    public static final int SPEED_OF_ELEVATOR = 1; // m/s
-    public static final int BEGINNING_FLOOR = 1;
-    public static final int HEIGHT_OF_ONE_FLOOR = 1; // m/s
-    public static final int TIME_FOR_CLOSING_DOOR = 3; // s
-    public static final int TIME_FOR_OPENING_DOOR = 3; // s
-    public static final int MILLISECONDS_IN_SECOND = 1000;
-    public static final int TIME_FOR_BOARDING = 3; // s
+    int NUMBER_OF_FLOORS = 4;
+    int SPEED_OF_ELEVATOR = 1; // m/s
+    int BEGINNING_FLOOR = 1;
+    int HEIGHT_OF_ONE_FLOOR = 1; // m/s
+    int TIME_FOR_CLOSING_DOOR = 3; // s
+    int TIME_FOR_OPENING_DOOR = 3; // s
+    int MILLISECONDS_IN_SECOND = 1000;
+    int TIME_FOR_BOARDING = 3; // s
 
 }
